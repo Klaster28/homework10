b12e6297db5d20187abe195a55f588ac80e5ab00
Klaster28 <89096914521@mail.ru>
2022-10-18 09:17:44 +0300

    Коммит после 1 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+    String firstName = " Иван ";
+    String middleName = " Иванович ";
+    String lastName = " Иванов ";
+    String fullName = lastName  + firstName  + middleName;
+        System.out.println("Ф.И.О. сотрудника  - " + fullName);
+
+        // Задание 2
+
 
     }
 }
